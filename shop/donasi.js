35f8e94653975bf0bb79b81e10beffab5f13d502
Futaroukun <93515103+Futaroukun@users.noreply.github.com>
2022-01-02 12:15:03 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,27 @@
 const donasibot = () => {
 return `DONASI BISA VIA BANK / E-MONEY
 
-*🏧 BANK*
+*PULSA*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+1. INDOSAT 
+	ㅁ 0857 2215 7719
+2. AXIS 
+	ㅁ 0838 5455 1575
+3. - 
+	ㅁ 
+4. -
+	ㅁ 
 	
-*💰 E-MONEY*
+*E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0838 5455 1575
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0838 5455 1575
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0857 2215 7719
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Ra**i
 `
 	}
 
